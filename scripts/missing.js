679e7d4c67a544973acdba8966608c9bbf1baa1c
Rocket Sloth <87209309+RocketSloth@users.noreply.github.com>
2023-01-19 12:24:03 -0600

    Update missing.js

--- scripts/missing.js
+++ scripts/missing.js
@@ -19,6 +19,9 @@ async function findMissingWords(addresses) {
   // Set a maximum number of iterations
   const maxIterations = 50000000;
 
+  // Create an empty object to store the new address and its corresponding phrase
+  let newAddress = {}
+
   // Try random combinations of words in the wordlist until a match is found or the maximum number of iterations is reached
   while (count < maxIterations) {
     // Create a phrase using random words from the wordlist
@@ -37,44 +40,51 @@ async function findMissingWords(addresses) {
       words[Math.floor(Math.random() * words.length)],
     ];
 
-    // Print the current phrase
-    //console.log(`Trying phrase: ${phrase.join(' ')}`);
-
-    // Add a delay of 1 second between each iteration
-    //await new Promise(resolve => setTimeout(resolve, 1));
-
     // Check if the resulting phrase creates any of the desired addresses
-    const keypair = await Keypair.fromWords(phrase).catch(() => {});
-    const newAddress = keypair ? keypair.address.b58 : [];
-    console.log(`newAddress: ${newAddress}`);
-    console.log(`iteration ${counter}`);
-    //console.log(`includes result: ${addresses.includes(newAddress)}`);
+    try {
+      const keypair = await Keypair.fromWords(phrase);
+      newAddress.address = keypair.address.b58;
+      newAddress.phrase = phrase;
+      console.log(`iteration ${counter}`);
+      console.log(`newAddress: ${newAddress.address}`);
+      console.log(`${newAddress.phrase}`);
+      
 
-    if (addresses.includes(newAddress)) {
-      // Save the match to a file
-      fs.appendFileSync('matches.txt', `${phrase.join(' ')}\n`);
-      // Return the missing words that create the desired address
-      return phrase;
+      if (addresses.includes(newAddress.address)) {
+        // Save the match to a file
+        fs.appendFileSync('matches.txt', `${newAddress.phrase.join(' ')}\n`);
+        // Return the new address and its corresponding phrase
+        return newAddress;
+      }
+    } catch (error) {
+      //console.log(error)
     }
-  // Increment the iteration count
-  count++;
-  counter++;
+    // Reset newAddress object
+    newAddress = {};
+    // Increment the iteration count
+    count++;
+    counter++;
   }
 
-  // If no combination of words is found to create the desired address, return an empty array
-  return [];
+  // If no combination of words is found to create the desired address, return an empty object
+  return {};
 }
 
-
+  
 async function findWord() {
-  // Call the findMissingWords() function to search for the missing words
-  const result = await findMissingWords(addresses);
-  if (result) {
-    log(`The missing words are: ${result.join(' ')}`);
-  }
-  else{
-    log(`Could not find the missing words.`);
+  try{
+    // Call the findMissingWords() function to search for the new address and its corresponding phrase
+    const result = await findMissingWords(addresses);
+    if (result.address) {
+      log(`The new address is: ${result.address}`);
+      log(`The corresponding phrase is: ${result.phrase.join(' ')}`);
+    } else {
+      log(`Could not find the new address and its corresponding phrase.`);
+    }
+  }catch(error){
+    //console.log(error)
   }
 }
-// Call the findWord() function to search for the missing words
+// Call the findWord() function to search for the new address and its corresponding phrase
 findWord().then(() => console.log('Done'));
+  
